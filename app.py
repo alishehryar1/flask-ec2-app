d268622903cf3679ea56a91d0debc097a7401873
alishehryar1 <alishehryar11@gmail.com>
2025-09-11 05:58:56 +0000

    Update templates + layout + static CSS

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Home Page</h1><p>Welcome to my Flask App!</p>"
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1><p>This is served from EC2 using Flask.</p>"
+    return render_template("about.html")
 
 @app.route("/contact")
 def contact():
-    return "<h1>Contact</h1><p>You can reach me at example@email.com</p>"
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
